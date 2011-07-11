423f02f888bcccedd2d86fe17839550df3d32fe4
Karthik N <karthigayan.n@gmail.com>
2011-07-11 14:09:16 +0100

    Tidy up

--- features/step_definitions/navigation.rb
+++ features/step_definitions/navigation.rb
@@ -96,6 +96,25 @@ def goto_home_page
   browser.goto($environment_url)
 end
 
+When /^I tick '(.*)'$/ do |name|
+  browser.checkbox(:name, name).click
+end
+
+Then /^I enter '(.*)' as '(.*?)'$/ do|field, value|
+  browser.text_field(:name, field).set(value)
+end
+
+When /^I select '(.*)' as '(.*)'$/ do |field_name, value|
+  browser.select_list(:name, field_name).select(value)
+end
+
+When /^I choose '(.*)' as '(.*)'$/ do |field_name, value|
+  browser.radio_button(field_name, value).set
+end
+
+When /^I check '(.*)' '(.*)'$/ do |name, value|
+  browser.checkbox(:name, name, value).click
+end
 
 When /^I (expand|collapse) the '(.*)' section$/ do |action,section_name|
   expand_section section_name
